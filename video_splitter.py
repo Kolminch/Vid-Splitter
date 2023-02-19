d13349260f170add835d7f58635f5e25059579f5
yaw.o.k <obengky72@gmail.com>
2023-02-19 23:32:27 +0000

    Update split time for testing

--- video_splitter.py
+++ video_splitter.py
@@ -1,7 +1,7 @@
 from ffmpeg_split import split_by_seconds
 import shutil
 
-def split(filename, seconds=30):
+def split(filename, seconds=2):
 	video_files = split_by_seconds('vid/input.mov', 2, 'h264')
 	return video_files
 
