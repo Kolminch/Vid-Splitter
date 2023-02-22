8492fb4e398fae62ba540efc2ad7612b5903fbc1
yaw.o.k <obengky72@gmail.com>
2023-02-22 22:13:17 +0000

    Fix changing video split size bug

--- settings.py
+++ settings.py
@@ -4,6 +4,3 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 # 30 second video split size by default
 SPLIT_SIZE = 30
-
-def change_split_size(new_split_size):
-    SPLIT_SIZE = new_split_size
--- video_splitter.py
+++ video_splitter.py
@@ -2,8 +2,16 @@ from ffmpeg_split import split_by_seconds
 import os
 import settings
 
+seconds = settings.SPLIT_SIZE
 
-def split(filename, seconds=settings.SPLIT_SIZE):
+
+def change_seconds(new_seconds):
+    global seconds
+    seconds = new_seconds
+    print(seconds)
+
+
+def split(filename, seconds=seconds):
     video_files = split_by_seconds(filename, seconds, "h264")
     return video_files
 
