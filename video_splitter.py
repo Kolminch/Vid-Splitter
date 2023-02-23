8d482d6004bba6288287f8f5b0245ce4bf8d92aa
yaw.o.k <obengky72@gmail.com>
2023-02-23 23:44:36 +0000

    Update to class

--- video_splitter.py
+++ video_splitter.py
@@ -2,20 +2,19 @@ from ffmpeg_split import split_by_seconds
 import os
 import settings
 
-seconds = settings.SPLIT_SIZE
 
+class Settings:
 
-def change_seconds(new_seconds):
-    global seconds
-    seconds = new_seconds
-    print(seconds)
+    def __init__(self):
+        self.seconds = settings.SPLIT_SIZE
 
+	def change_seconds(self, new_seconds):
+		self.seconds = new_seconds
 
-def split(filename, seconds=seconds):
-    video_files = split_by_seconds(filename, seconds, "h264")
-    return video_files
+	def split(self, filename, seconds=self.seconds):
+		video_files = split_by_seconds(filename, seconds, "h264")
+		return video_files
 
-
-def remove(files):
-    for file in files:
-        os.remove(file)
+	def remove(files):
+		for file in files:
+			os.remove(file)
